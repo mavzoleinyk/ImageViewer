57e3c2180cf42090d1d9f948695e19fcaae89cd4
Vitalii <itstart.com.ua@gmail.com>
2022-09-25 13:17:52 +0300

    add fix 2

--- fMain.pas
+++ fMain.pas
@@ -20,8 +20,11 @@ type
     Separator1: TMenuItem;
     Open1: TOpenDialog;
     Save1: TSaveDialog;
+    procedure FormCreate(Sender: TObject);
     procedure MenuItem3Click(Sender: TObject);
     procedure MenuItem4Click(Sender: TObject);
+    procedure MenuItem5Click(Sender: TObject);
+    procedure MenuItem6Click(Sender: TObject);
   private
 
   public
@@ -48,12 +51,39 @@ begin
   Image1.Show;
 end;
 
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  str: String; //путь к файлу
+  i: Integer;
+begin
+
+  if(ParamCount > 0) then
+    begin
+      str := ParamStr(1);
+      for i := 2 to ParamCount do
+        str := str + ' ' + ParamStr(i);
+      Image1.Picture.LoadFromFile(str);
+      Image1.Show;
+    end;
+
+end;
+
 procedure TForm1.MenuItem4Click(Sender: TObject);
 begin
   if Save1.Execute then
     Image1.Picture.SaveToFile(Save1.FileName)
   else
     ShowMessage('Не сохранено!');
+end;
+
+procedure TForm1.MenuItem5Click(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TForm1.MenuItem6Click(Sender: TObject);
+begin
+  Image1.Hide; //прячем
 end;
 
 end.
